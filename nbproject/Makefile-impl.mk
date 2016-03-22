be3bb8ac919ed6dffa937b92aaca447d346ed78b
VladimirTheGreat <sviderskijvladimir@gmail.com>
2016-03-22 10:13:13 +0100

    Revert "TEST2!"
    This reverts commit 1a2d02c56c8e96b5aa4f91a9cc6f96c0d0a2a781.

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=TempHumMeterProject
+PROJECTNAME=picTest.X
 
 # Active Configuration
 DEFAULTCONF=default
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=TempHumMeterProject.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/TempHumMeterProject.production.hex
+CND_ARTIFACT_NAME_default=picTest.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/picTest.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=temphummeterproject.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/temphummeterproject.tar
+CND_PACKAGE_NAME_default=pictest.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pictest.x.tar
